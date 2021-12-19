271b142fd6bb173f92bf520746297dca74d8629c
Crlotwhite <crlotwhite@gmail.com>
2021-12-19 23:21:51 +0800

    Add ChatRoom Model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class ChatRoomManager(models.Manager):
+    # ref: https://stackoverflow.com/questions/9415616/adding-to-the-constructor-of-a-django-model
+    def create_chatroom(self, name):
+        chat_room = self.create(name=name)
+        return chat_room
+
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=32)
+    secrets = models.CharField(max_length=32)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'Chat room\'s name {self.name}'
+
+    objects = ChatRoomManager()
