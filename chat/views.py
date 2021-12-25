5e2e489c4590d20051e2c872754c31cefbc40e71
Crlotwhite <crlotwhite@gmail.com>
2021-12-25 18:31:41 +0800

    Add frontend pages

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 
 
-# Create your views here.
 def index(request):
+    """
+    First Function View as an entrance
+    """
     return render(request, 'index.html')
 
 
-def room(request, room_name):
-    return render(
-        request,
-        'room.html',
-        {'room_name': room_name}
-    )
+def room(request):
+    """
+    it is chat service view
+    - show room list.
+    - start chat in the room
+    """
+
+    if request.method == 'POST':
+        # when you enter specific room
+        from .models import ChatRoom
+
+        room_name = request.POST['room_name']
+        chatroom = ChatRoom.objects.get(name=room_name)
+        return render(
+            request,
+            'chatroom.html',
+            {
+                'room_name': chatroom.name,
+                'secret': chatroom.secrets,
+            }
+        )
+    else:
+        # if other method same function like GET request.
+        from .models import ChatRoom
+
+        return render(
+            request,
+            'room_list.html',
+            {'room_list': [x.name for x in ChatRoom.objects.all()]}
+        )
